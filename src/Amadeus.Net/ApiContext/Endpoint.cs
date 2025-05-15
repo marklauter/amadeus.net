05b2eaa63c62f44adde05fd2de47df184d46185f
Mark Lauter <mark@marklauter.com>
2025-05-15 00:26:21 -0400

    made the filter required

--- src/Amadeus.Net/ApiContext/Endpoint.cs
+++ src/Amadeus.Net/ApiContext/Endpoint.cs
@@ -3,14 +3,14 @@ using LanguageExt;
 
 namespace Amadeus.Net.ApiContext;
 
-public sealed class Endpoint<TResult, TFilter>(Func<Option<TFilter>, CancellationToken, Task<Either<ErrorResponse, TResult>>> readFunc)
+public sealed class Endpoint<TResult, TFilter>(Func<TFilter, CancellationToken, Task<Either<ErrorResponse, TResult>>> readFunc)
 {
-    private readonly Func<Option<TFilter>, CancellationToken, Task<Either<ErrorResponse, TResult>>> readFunc =
+    private readonly Func<TFilter, CancellationToken, Task<Either<ErrorResponse, TResult>>> readFunc =
         readFunc ?? throw new ArgumentNullException(nameof(readFunc));
 
-    public EndpointQuery<TResult, TFilter> FilterBy(Func<Option<TFilter>> filterFactory) =>
+    public EndpointQuery<TResult, TFilter> FilterBy(Func<TFilter> filterFactory) =>
         new(this, filterFactory);
 
-    public Task<Either<ErrorResponse, TResult>> ReadAsync(Option<TFilter> filter, CancellationToken cancellationToken) =>
+    internal Task<Either<ErrorResponse, TResult>> ReadAsync(TFilter filter, CancellationToken cancellationToken) =>
         readFunc(filter, cancellationToken);
 }
--- src/Amadeus.Net/ApiContext/EndpointQuery.cs
+++ src/Amadeus.Net/ApiContext/EndpointQuery.cs
@@ -6,9 +6,9 @@ namespace Amadeus.Net.ApiContext;
 public sealed class EndpointQuery<TResult, TFilter>
 {
     private readonly Endpoint<TResult, TFilter> endpoint;
-    private readonly Func<Option<TFilter>> filterFactory;
+    private readonly Func<TFilter> filterFactory;
 
-    internal EndpointQuery(Endpoint<TResult, TFilter> endpoint, Func<Option<TFilter>> filterFactory)
+    internal EndpointQuery(Endpoint<TResult, TFilter> endpoint, Func<TFilter> filterFactory)
     {
         this.endpoint = endpoint;
         this.filterFactory = filterFactory;
