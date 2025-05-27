1336c8b5d108c618278a68cda0489da40d1282ff
Mark Lauter <mark@marklauter.com>
2025-05-27 16:33:40 -0400

    for and await not required

--- src/Amadeus.Net/ApiContext/IOExtensions.cs
+++ src/Amadeus.Net/ApiContext/IOExtensions.cs
@@ -5,8 +5,8 @@ namespace Amadeus.Net.ApiContext;
 
 public static class IOExtensions
 {
-    public static ValueTask<Either<ErrorResponse, R>> GetAsync<R>(
+    public static ValueTask<Either<ErrorResponse, R>> InvokeAsync<R>(
         this IO<Either<ErrorResponse, R>> operation,
         CancellationToken cancellationToken) =>
-            operation.Fork().Await().RunAsync(EnvIO.New(token: cancellationToken));
+            operation.RunAsync(EnvIO.New(token: cancellationToken));
 }
