d55ecd48ed4ed3ffa68ad8152ec4caf54b619901
Mark Lauter <mark@marklauter.com>
2025-05-27 16:34:36 -0400

    added test

---
+++ src/Amadeus.Net.Tests/Clients/FlightInspirationClientTests.cs
@@ -0,0 +1,40 @@
+﻿using Amadeus.Net.ApiContext;
+using Amadeus.Net.Clients.FlightInspiration;
+using LanguageExt;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Amadeus.Net.Tests.Clients;
+
+public sealed class FlightInspirationClientTests
+{
+    private readonly IConfiguration configuration = new ConfigurationBuilder()
+        .AddInMemoryCollection(new Dictionary<string, string>
+        {
+            { "Amadeus:Host", "https://test.api.amadeus.com" },
+            { "Amadeus:ClientVersion", "0.0.0" },
+            { "Amadeus:ClientName", "TWAI" },
+        }!)
+        .AddUserSecrets(Assembly.GetExecutingAssembly(), true, false)
+        .Build();
+
+    [Fact]
+    public async Task ContextTest()
+    {
+        var tkn = CancellationToken.None;
+        var response =
+            await Prelude.use(
+                acquire: () => new ServiceCollection()
+                    .AddAmadeusContext(configuration)
+                    .BuildServiceProvider(),
+                release: provider => provider.Dispose())
+            .Map(provider => provider.GetRequiredService<AmadeusContext>())
+            .Bind(context => context.FlightInspirations.Filter(FlightInspirationFilter.From("PAR")))
+            .InvokeAsync(tkn);
+
+        _ = response.Match(
+            Left: error => Assert.Fail("expected success"),
+            Right: r => Assert.True(r.Destinations.Any()));
+    }
+}
