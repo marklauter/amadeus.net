f9331fb5bf482d79d1af567b00df612d5f540b63
Mark Lauter <mark@marklauter.com>
2025-05-27 22:28:59 -0400

    better names

--- src/Amadeus.Net/ApiContext/AmadeusContext.cs
+++ src/Amadeus.Net/ApiContext/AmadeusContext.cs
@@ -6,7 +6,6 @@ using Amadeus.Net.Clients.FlightInspiration;
 using Amadeus.Net.Clients.FlightInspiration.Response;
 using Amadeus.Net.HttpClientExtensions;
 using Amadeus.Net.Options;
-using LanguageExt;
 
 namespace Amadeus.Net.ApiContext;
 
@@ -17,37 +16,37 @@ public sealed class AmadeusContext(
     private readonly Lazy<Endpoint<AirlineCodeQuery, AirlineLookupResponse>> airlines =
         new(() =>
             new Endpoint<AirlineCodeQuery, AirlineLookupResponse>(
-                Get: filter => httpClient.Filter<AirlineCodeQuery, AirlineLookupResponse>(
+                Get: query => httpClient.Get<AirlineCodeQuery, AirlineLookupResponse>(
                     options,
                     AirlineCodeLookupClient.Path,
-                    filter)));
+                    query)));
     public Endpoint<AirlineCodeQuery, AirlineLookupResponse> Airlines => airlines.Value;
 
     private readonly Lazy<Endpoint<FlightInspirationQuery, FlightInspirationResponse>> flightInspirations =
         new(() =>
             new Endpoint<FlightInspirationQuery, FlightInspirationResponse>(
-                Get: filter => httpClient.Filter<FlightInspirationQuery, FlightInspirationResponse>(
+                Get: query => httpClient.Get<FlightInspirationQuery, FlightInspirationResponse>(
                     options,
                     FlightInspirationClient.Path,
-                    filter)));
+                    query)));
     public Endpoint<FlightInspirationQuery, FlightInspirationResponse> FlightInspirations => flightInspirations.Value;
 
     private readonly Lazy<Endpoint<AirportCityQuery, AirportCitySearchResponse>> airportCities =
         new(() =>
             new Endpoint<AirportCityQuery, AirportCitySearchResponse>(
-                    Get: filter => httpClient.Filter<AirportCityQuery, AirportCitySearchResponse>(
+                    Get: query => httpClient.Get<AirportCityQuery, AirportCitySearchResponse>(
                         options,
                         AirportCitySearchClient.Path,
-                        filter)));
+                        query)));
     public Endpoint<AirportCityQuery, AirportCitySearchResponse> AirportCities => airportCities.Value;
 
     private readonly Lazy<Endpoint<LocationId, Location>> airportCity =
         new(() =>
             new Endpoint<LocationId, Location>(
-                    Get: locationId => httpClient.Filter<LocationId, Location>(
+                    Get: query => httpClient.Get<LocationId, Location>(
                         options,
-                        $"{AirportCitySearchClient.Path}/{locationId}",
-                        locationId)));
+                        $"{AirportCitySearchClient.Path}/{query}",
+                        query)));
     public Endpoint<LocationId, Location> AirportCity => airportCity.Value;
 }
 
